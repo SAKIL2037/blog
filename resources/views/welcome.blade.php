e622b58e261f678c625c1caeca9f8cb1792e0868
Sakil Mia <34392752+SAKIL2037@users.noreply.github.com>
2021-03-10 19:05:33 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel sakil mia
                 </div>
 
                 <div class="links">
